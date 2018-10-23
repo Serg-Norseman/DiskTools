ca76af518ff13d5bf13b2d154cff6d1d3465801b
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2018-10-23 23:21:09 +0300

    Project's blank

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FileChecker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("FileChecker")]
+[assembly: AssemblyCopyright("Copyright (C) 2018 by Sergey V. Zhdanovskih")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
---
+++ clean.cmd
@@ -0,0 +1,7 @@
+rmdir .\.vs /s /q
+
+rmdir .\bin /s /q
+rmdir .\obj /s /q
+rmdir .\ProfilingSessions /s /q
+
+del .\*.zip /q
